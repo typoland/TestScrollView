9bc76e4603c3dd1a0046c6f084ffef84483f8bdd
Łukasz Dziedzic <dziedzic@alfabety.pl>
2017-12-13 12:05:40 +0100

    Initial Commit

---
+++ ZoomWindowTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ZoomWindowTest
+//
+//  Created by Łukasz Dziedzic on 13/12/2017.
+//  Copyright © 2017 Łukasz Dziedzic. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ZoomWindowTest/Document.swift
@@ -0,0 +1,28 @@
+//
+//  Document.swift
+//  ZoomWindowTest
+//
+//  Created by Łukasz Dziedzic on 13/12/2017.
+//  Copyright © 2017 Łukasz Dziedzic. All rights reserved.
+//
+
+import Cocoa
+
+class Document: NSPersistentDocument {
+
+    override init() {
+        super.init()
+        // Add your subclass-specific initialization here.
+    }
+
+    override class var autosavesInPlace: Bool {
+        return true
+    }
+
+    override var windowNibName: NSNib.Name? {
+        // Returns the nib file name of the document
+        // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this property and override -makeWindowControllers instead.
+        return NSNib.Name("Document")
+    }
+
+}
